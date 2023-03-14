0920b9e0430f6de0027185fe15f660298fd92523
MAjay2 <adjeimichael405@gmail.com>
2023-03-14 13:31:02 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+
+function App(){
+  return(
+  <>
+    <h1 style={{backgroundColor:"red"}}>Codetrain is awesome, React works!!!</h1>
+  
+  </>
+  )
+}
+
+export default App;
